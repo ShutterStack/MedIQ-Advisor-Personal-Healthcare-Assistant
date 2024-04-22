d4ad0881fe54784780ae7b6c4f861e00dff97b07
Himanshu Maurya <himanshu.m1802@gmail.com>
2024-04-22 13:27:14 +0530

    file templates

---
+++ Virtual Assistance-final project/www/controller.js
@@ -0,0 +1,53 @@
+$(document).ready(function () {
+
+
+
+    // Display Speak Message
+    eel.expose(DisplayMessage)
+    function DisplayMessage(message) {
+
+        $(".siri-message li:first").text(message);
+        $('.siri-message').textillate('start');
+
+    }
+
+    // Display hood
+    eel.expose(ShowHood)
+    function ShowHood() {
+        $("#Oval").attr("hidden", false);
+        $("#SiriWave").attr("hidden", true);
+    }
+
+    eel.expose(senderText)
+    function senderText(message) {
+        var chatBox = document.getElementById("chat-canvas-body");
+        if (message.trim() !== "") {
+            chatBox.innerHTML += `<div class="row justify-content-end mb-4">
+            <div class = "width-size">
+            <div class="sender_message">${message}</div>
+        </div>`; 
+    
+            // Scroll to the bottom of the chat box
+            chatBox.scrollTop = chatBox.scrollHeight;
+        }
+    }
+
+    eel.expose(receiverText)
+    function receiverText(message) {
+
+        var chatBox = document.getElementById("chat-canvas-body");
+        if (message.trim() !== "") {
+            chatBox.innerHTML += `<div class="row justify-content-start mb-4">
+            <div class = "width-size">
+            <div class="receiver_message">${message}</div>
+            </div>
+        </div>`; 
+    
+            // Scroll to the bottom of the chat box
+            chatBox.scrollTop = chatBox.scrollHeight;
+        }
+        
+    }
+
+
+});
